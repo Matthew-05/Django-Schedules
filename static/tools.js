fea39a3171d70ff385d01bf589708e6c987ac268
xheer <83892518+xheer@users.noreply.github.com>
2023-08-23 17:50:43 -0700

    fixed modals breaking navbar

--- static/tools.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  window.addEventListener("resize", () => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    console.log(`The viewport's width is ${width} and the height is ${height}.`);
-  })
-});
